c534bb13d77313a07efa6346e101e991ab82c769
Caleb James Smith <colorado.caleb.smith@gmail.com>
2021-07-26 16:10:00 -0500

    Add xml files and script for running BERT scan.

---
+++ scripts/BERT_Scan.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+# BERT_Scan.sh
+
+echo "Running BERT scan."
+
+TAP0_Setting=$1
+dataDir="BERT_Scan_output"
+
+if [ -z "$TAP0_Setting" ]
+then
+    echo " - Please provide a TAP0 setting [0,1000] as the first argument."
+    echo " - Note that communication fails for low values... e.g. TAP0 less than 10."
+    exit 1
+fi
+
+mkdir -p "$dataDir"
+
+
+# Set CML_TAP0_BIAS in config file:
+
+# using sed with variables and quotes:
+# https://askubuntu.com/questions/76808/how-do-i-use-variables-in-a-sed-command
+sed -e "s|CML_TAP0_BIAS           =   \"500\"|CML_TAP0_BIAS           =   \""${TAP0_Setting}"\"|g" CMSIT_BERT_Scan.xml > CMSIT_BERT_Scan_Custom.xml
+
+grep CML_TAP0_BIAS CMSIT_BERT_Scan_Custom.xml
+
+CMSITminiDAQ -f CMSIT_BERT_Scan_Custom.xml -c bertest > "$dataDir/scan.log"
+
+grep Final "$dataDir/scan.log"
+
