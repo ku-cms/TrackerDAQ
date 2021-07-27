c406220c7df500854eded60107fc7988aa85ecf6
Caleb James Smith <colorado.caleb.smith@gmail.com>
2021-07-27 12:09:29 -0500

    Add real data values from scan to example

--- python/BERT_Plot.py
+++ python/BERT_Plot.py
@@ -3,17 +3,17 @@
 import matplotlib.pyplot as plt
 import tools
 
-def plot(plot_dir, output_file):
+def plot(plot_dir, output_file, x_values, y_values):
     tools.makeDir(plot_dir)
 
     fig, ax = plt.subplots(figsize=(6, 6))
     
-    plt.plot([50, 60, 70, 80, 90, 100], [10**6, 10**4, 10**2, 0, 0, 0], 'ro')
+    plt.plot(x_values, y_values, 'ro')
 
     ax.set_yscale('symlog')
-    ax.set_title("BERT Scan",   fontsize=20)
-    ax.set_xlabel("TAP0",       fontsize=16)
-    ax.set_ylabel("Errors",     fontsize=16)
+    ax.set_title("BERT TAP0 Scan",          fontsize=20)
+    ax.set_xlabel("TAP0 DAC Setting",       fontsize=16)
+    ax.set_ylabel("Errors (over 10 s)",     fontsize=16)
     
     output_png = "{0}/{1}.png".format(plot_dir, output_file)
     output_pdf = "{0}/{1}.pdf".format(plot_dir, output_file)
@@ -24,7 +24,9 @@ def plot(plot_dir, output_file):
 def main():
     plot_dir    = "plots"
     output_file = "BERT_example"
-    plot(plot_dir, output_file)
+    x_values = [50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+    y_values = [179652, 9469, 192, 0, 0, 0, 0, 0, 0, 0, 0]
+    plot(plot_dir, output_file, x_values, y_values)
 
 if __name__ == "__main__":
     main()
