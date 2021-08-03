ec4e7fd6124b17c817171f68fee44a17663f688e
Caleb James Smith <colorado.caleb.smith@gmail.com>
2021-08-03 16:14:50 -0500

    BERT scan: add data dir as argument; use options in python script.

--- python/BERT_Scan.py
+++ python/BERT_Scan.py
@@ -1,21 +1,37 @@
 # BERT_Scan.py
 
 import subprocess
+import argparse
+
 
 # Scan over TAP0 DAQ settings
-def run(tap0_min, tap0_max, tap0_step):
+def run(tap0_min, tap0_max, tap0_step, output_dir):
     for x in range(tap0_min, tap0_max + tap0_step, tap0_step):
-        # Use BERT scan script
-        output = subprocess.run(["./TrackerDAQ/scripts/BERT_Scan.sh", str(x)])
+        # Run BERT scan script
+        output = subprocess.run(["./TrackerDAQ/scripts/BERT_Scan.sh", str(x), output_dir])
         print(output)
-        #output = subprocess.check_output(["./TrackerDAQ/scripts/BERT_Scan.sh", "100"])
-        #print(output)
 
 def main():
-    tap0_min  = 50
-    tap0_max  = 600
-    tap0_step = 10
-    run(tap0_min, tap0_max, tap0_step)
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--tap0_min",     "-a",   default=-1,   help="Minimum TAP0 value")
+    parser.add_argument("--tap0_max",     "-b",   default=-1,   help="Maximum TAP0 value")
+    parser.add_argument("--tap0_step",    "-c",   default=-1,   help="Step size for TAP0")
+    parser.add_argument("--output_dir",   "-d",   default="",   help="output directory")
+    
+    options     = parser.parse_args()
+    tap0_min    = int(options.tap0_min)
+    tap0_max    = int(options.tap0_max)
+    tap0_step   = int(options.tap0_step)
+    output_dir  = options.output_dir
+    
+    if tap0_min < 0 or tap0_max < 0 or tap0_step < 0:
+        print("Provide TAP0 DAC min, max, and step (options -a, -b, and -c). The allowed TAP0 range is [0, 1000].")
+        return
+    if not output_dir:
+        print("Provide and output directory with the -d option.")
+        return
+    
+    run(tap0_min, tap0_max, tap0_step, output_dir)
 
 if __name__ == "__main__":
     main()
--- scripts/BERT_Scan.sh
+++ scripts/BERT_Scan.sh
@@ -1,33 +1,35 @@
 #!/usr/bin/bash
 # BERT_Scan.sh
 
-echo "Running BERT scan."
-
 TAP0_Setting=$1
-#dataDir="BERT_Scan_SingleDP_Data"
-#dataDir="BERT_Scan_DoubleDP_DoubleBonn_Data"
-dataDir="BERT_Scan_DoubleDP_DoubleBonn_DoubleYellow_elink101_Data"
-#dataDir="BERT_Scan_DoubleDP_DoubleBonn_DoubleYellow_elink102_Data"
-#dataDir="BERT_Scan_DoubleDP_DoubleBonn_DoubleYellow_elink104_Data"
-#dataDir="BERT_Scan_DoubleDP_DoubleBonn_DoubleYellow_elink106_Data"
-#dataDir="BERT_Scan_DoubleDP_DoubleBonn_DoubleYellow_elink111_Data"
-#dataDir="BERT_Scan_DoubleDP_DoubleBonn_DoubleYellow_elink113_Data"
+dataDir=$2
+
+# Check that required arguments have been provided
 
 if [ -z "$TAP0_Setting" ]
 then
     echo " - Please provide a TAP0 setting [0,1000] as the first argument."
-    echo " - Note that communication fails for low values... e.g. TAP0 less than 10."
+    echo " - Note that communication fails for low values... e.g. TAP0 less than 50."
+    exit 1
+fi
+
+if [ -z "$dataDir" ]
+then
+    echo " - Please provide a directory as the second argument."
     exit 1
 fi
 
 mkdir -p "$dataDir"
-
 
 # Set CML_TAP0_BIAS in config file:
 
 # using sed with variables and quotes:
 # https://askubuntu.com/questions/76808/how-do-i-use-variables-in-a-sed-command
 sed -e "s|CML_TAP0_BIAS           =   \"500\"|CML_TAP0_BIAS           =   \""${TAP0_Setting}"\"|g" CMSIT_BERT_Scan.xml > CMSIT_BERT_Scan_Custom.xml
+
+# Run BERT
+
+echo "Running BERT with TAP0=$TAP0_Setting"
 
 grep CML_TAP0_BIAS CMSIT_BERT_Scan_Custom.xml
 
