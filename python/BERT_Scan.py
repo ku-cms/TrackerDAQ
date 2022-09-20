4ccbad5fc5c18da5c2dcf1db8932a910b8780d82
Caleb James Smith <colorado.caleb.smith@gmail.com>
2022-09-20 15:10:22 -0500

    testing diff firmware versions

--- python/BERT_Scan.py
+++ python/BERT_Scan.py
@@ -8,12 +8,13 @@ import os
 def getDefaultInputs(cable_number, channel):
     # Default input parameters
     inputs      = {}
-    tap0_min    = 350
-    tap0_max    = 450
-    tap0_step   = 10
+    tap0_min    = 50
+    tap0_max    = 100
+    tap0_step   = 5
     signal      = 0
+    output_dir  = "BERT_TAP0_Scans/SingleDP/CERN_FMC_FC7_FW_v4.2"
     #output_dir  = "BERT_TAP0_Scans/DoubleDP_DPAdapter/elink{0}_SS{1}".format(cable_number, signal)
-    output_dir  = "BERT_TAP0_Scans/KSU_FMC_DoubleDP_DPAdapter/elink{0}_{1}_SS{2}".format(cable_number, channel, signal)
+    #output_dir  = "BERT_TAP0_Scans/KSU_FMC_DoubleDP_DPAdapter/elink{0}_{1}_SS{2}".format(cable_number, channel, signal)
     inputs["tap0_min"]      = tap0_min
     inputs["tap0_max"]      = tap0_max
     inputs["tap0_step"]     = tap0_step
