c404179e684ef9f1d9ed4a8ffceb8a1a3c1da7af
Caleb James Smith <colorado.caleb.smith@gmail.com>
2022-09-21 14:42:43 -0500

    Copy plots from new RD53A area.

--- scripts/getPlots.sh
+++ scripts/getPlots.sh
@@ -3,5 +3,6 @@
 
 # kucms-01
 rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/update/Ph2_ACF/DAQSettings_v1/plots/ raw_plots_RD53A
+rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/RD53A/Ph2_ACF/DAQSettings_v1/plots/ raw_plots_RD53A_v2
 rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/croc/Ph2_ACF/DAQSettings_v1/plots/ raw_plots_RD53B
 
