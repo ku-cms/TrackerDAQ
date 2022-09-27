cdb979560d0a779f39be6a8c240ef11fa3aa5da2
Caleb James Smith <colorado.caleb.smith@gmail.com>
2022-09-27 17:06:53 -0500

    Copy RD53A data from new area.

--- scripts/getData.sh
+++ scripts/getData.sh
@@ -2,8 +2,14 @@
 # getData.sh
 
 # kucms-01
+
+# data files
 rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/update/Ph2_ACF/DAQSettings_v1/data/BERT_TAP0_Scans/ raw_data_RD53A
+rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/RD53A/Ph2_ACF/DAQSettings_v1/data/BERT_TAP0_Scans/ raw_data_RD53A_v2
 rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/croc/Ph2_ACF/DAQSettings_v1/data/BERT_TAP0_Scans/ raw_data_RD53B
+
+# output tables
 rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/update/Ph2_ACF/DAQSettings_v1/output/ raw_output_RD53A
+rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/RD53A/Ph2_ACF/DAQSettings_v1/output/ raw_output_RD53A_v2
 rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/croc/Ph2_ACF/DAQSettings_v1/output/ raw_output_RD53B
 
