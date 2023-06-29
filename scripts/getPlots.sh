b1a5a4d584b5f7743e29fd804e07cc8c49ffa98b
Caleb James Smith <colorado.caleb.smith@gmail.com>
2023-06-29 10:13:59 -0500

    For get plots, update paths for port card data.

--- scripts/getPlots.sh
+++ scripts/getPlots.sh
@@ -2,7 +2,10 @@
 # getPlots.sh
 
 # kucms-01
-rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/update/Ph2_ACF/DAQSettings_v1/plots/ raw_plots_RD53A
-rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/RD53A/Ph2_ACF/DAQSettings_v1/plots/ raw_plots_RD53A_v2
-rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/croc/Ph2_ACF/DAQSettings_v1/plots/ raw_plots_RD53B
+#rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/update/Ph2_ACF/DAQSettings_v1/plots/ raw_plots_RD53A
+#rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/RD53A/Ph2_ACF/DAQSettings_v1/plots/ raw_plots_RD53A_v2
+#rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/croc/Ph2_ACF/DAQSettings_v1/plots/ raw_plots_RD53B
+
+rsync -az kucms@kucms.phsx.ku.edu:/home/kucms/TrackerDAQ/croc/Ph2_ACF/DAQSettings_v3/plots/ raw_plots_PortCard_RD53B
+rsync -az kucms@kucms.phsx.ku.edu:/home/kucms/TrackerDAQ/update/Ph2_ACF/DAQSettings_v1/plots/ raw_plots_RD53A
 
