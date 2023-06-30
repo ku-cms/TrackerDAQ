e69f255f958cbf32f703ab403594332ba49dc755
Caleb James Smith <colorado.caleb.smith@gmail.com>
2023-06-30 09:25:22 -0500

    Add measure() function.

--- python/measureTemperature.py
+++ python/measureTemperature.py
@@ -2,10 +2,14 @@
 
 from tools import getTempRD53B
 
-def main():
+def measure():
+    # Get temperature for RD53B; input: voltage (mV), output: temperature (C)
     voltage = int(input("Enter a positive integer voltage (mV): "))
     temperature = getTempRD53B(voltage)
     print("Temperature (C): {0:.2f}".format(temperature))
+
+def main():
+    measure()
 
 if __name__ == "__main__":
     main()
