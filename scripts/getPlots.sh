03480aa87028ff50e39ce23f2701147b1d0c9904
Caleb James Smith <colorado.caleb.smith@gmail.com>
2023-11-09 14:22:54 -0600

    add comment for kucms rsync commands

--- scripts/getPlots.sh
+++ scripts/getPlots.sh
@@ -6,6 +6,7 @@
 #rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/RD53A/Ph2_ACF/DAQSettings_v1/plots/ raw_plots_RD53A_v2
 #rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/croc/Ph2_ACF/DAQSettings_v1/plots/ raw_plots_RD53B
 
+# kucms
 rsync -az kucms@kucms.phsx.ku.edu:/home/kucms/TrackerDAQ/croc/Ph2_ACF/DAQSettings_v3/plots/ raw_plots_PortCard_RD53B
 rsync -az kucms@kucms.phsx.ku.edu:/home/kucms/TrackerDAQ/update/Ph2_ACF/DAQSettings_v1/plots/ raw_plots_RD53A
 
