c28241dedc3ad1a6e7ddb233d110209641fc6049
Caleb James Smith <colorado.caleb.smith@gmail.com>
2024-01-02 17:41:44 -0600

    Setup script: create symbolic links

--- scripts/SetupTrackerDAQ.sh
+++ scripts/SetupTrackerDAQ.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 # SetupTrackerDAQ.sh
 
+INSTALL_DIR=/home/kucms/TrackerDAQ/TrackerDAQ
 TARGET_DIR=TrackerDAQ
 
 # Check if the directory already exists; if it does, exit.
@@ -17,5 +18,17 @@ echo "Creating the directory '$TARGET_DIR' and subdirectories."
 mkdir $TARGET_DIR
 mkdir $TARGET_DIR/python
 mkdir $TARGET_DIR/scripts
+
+echo "Creating symbolic links."
+
+cd $TARGET_DIR/python
+ln -s $INSTALL_DIR/python/BERT_Run_Scan.py .
+ln -s $INSTALL_DIR/python/BERT_Simple_Analyze.py .
+cd ../..
+
+cd $TARGET_DIR/scripts
+ln -s $INSTALL_DIR/scripts/PortCard_BERT_Scan.sh .
+ln -s $INSTALL_DIR/scripts/analyze_RD53B.sh .
+cd ../..
 
 
