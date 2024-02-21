c4f848c19b400ca721da7395533ea41bb482de69
Caleb James Smith <colorado.caleb.smith@gmail.com>
2024-02-21 14:09:46 -0600

    Since we are actually loading the canvas, rename histogram variables to canvas.

--- macros/analyze_occupancy.C
+++ macros/analyze_occupancy.C
@@ -1,7 +1,7 @@
 void analyze_occupancy()
 {
     // Input ROOT file name
-    //string input_file = "example_file.root";
+    // string input_file = "example_file.root";
     string input_file = "Results/Run000201_Physics.root";
     
     cout << "Running occupancy analysis on this input file: " << input_file << endl;
@@ -16,26 +16,27 @@ void analyze_occupancy()
         return;
     }
     
-    // Load 2D occupancy histogram
-    //string occ_2d_hist_name = "example_histogram";
-    string occ_2d_hist_name = "Detector/Board_0/OpticalGroup_0/Hybrid_0/Chip_15/D_B(0)_O(0)_H(0)_Occ2D_Chip(15)";
-    TH2F *occ_2d_hist = (TH2F*) f->Get(occ_2d_hist_name.c_str());
+    // Load 2D occupancy canvas
+    // string occ_2d_canvas_name = "example_canvas";
+    string occ_2d_canvas_name = "Detector/Board_0/OpticalGroup_0/Hybrid_0/Chip_15/D_B(0)_O(0)_H(0)_Occ2D_Chip(15)";
+    TCanvas *occ_2d_canvas = (TCanvas*)f->Get(occ_2d_canvas_name.c_str());
 
-    // Check for errors when loading histogram
-    if (!occ_2d_hist)
+    // Check for errors when loading canvas
+    if (!occ_2d_canvas)
     {
-        cerr << "ERROR: Unable to load this 2D histogram: " << occ_2d_hist_name << endl;
+        cerr << "ERROR: Unable to load this canvas: " << occ_2d_canvas_name << endl;
         f->Close();
         return;
     }
 
-    cout << "occ_2d_hist: " << occ_2d_hist << endl;
-    cout << "occ_2d_hist type: " << typeid(occ_2d_hist).name() << endl;
+    cout << "occ_2d_canvas: " << occ_2d_canvas << endl;
+    cout << "occ_2d_canvas type: " << typeid(occ_2d_canvas).name() << endl;
 
-    Int_t n_bins_x = occ_2d_hist->GetNbinsX();
-    Int_t n_bins_y = occ_2d_hist->GetNbinsY();
-    cout << "n_bins_x: " << n_bins_x << endl;
-    cout << "n_bins_y: " << n_bins_y << endl;
-    
-    occ_2d_hist->Draw();
+    // Int_t n_bins_x = occ_2d_hist->GetNbinsX();
+    // Int_t n_bins_y = occ_2d_hist->GetNbinsY();
+    // cout << "n_bins_x: " << n_bins_x << endl;
+    // cout << "n_bins_y: " << n_bins_y << endl;
+
+    // Draw canvas
+    occ_2d_canvas->Draw();
 }
--- python/analyze_occupancy.py
+++ python/analyze_occupancy.py
@@ -4,6 +4,7 @@ import ROOT
 
 def main():
     # Input ROOT file name
+    # input_file = "example_file.root"
     input_file = "Results/Run000201_Physics.root"
 
     print(f"Running occupancy analysis on this input file: {input_file}")
@@ -16,25 +17,29 @@ def main():
         print(f"ERROR: Unable to load this ROOT file: {input_file}")
         return
     
-    # Load 2D occupancy histogram
-    occ_2d_hist_name = "Detector/Board_0/OpticalGroup_0/Hybrid_0/Chip_15/D_B(0)_O(0)_H(0)_Occ2D_Chip(15)"
-    occ_2d_hist = f.Get(occ_2d_hist_name)
+    # Load 2D occupancy canvas
+    # occ_2d_canvas_name = "example_canvas"
+    occ_2d_canvas_name = "Detector/Board_0/OpticalGroup_0/Hybrid_0/Chip_15/D_B(0)_O(0)_H(0)_Occ2D_Chip(15)"
+    occ_2d_canvas = f.Get(occ_2d_canvas_name)
 
-    if not occ_2d_hist:
-        print(f"ERROR: Unable to load this 2D histogram: {occ_2d_hist_name}")
+    # Check for errors when loading canvas
+    if not occ_2d_canvas:
+        print(f"ERROR: Unable to load this canvas: {occ_2d_canvas_name}")
         f.Close()
         return
     
-    print(f"occ_2d_hist: {occ_2d_hist}")
-    print(f"occ_2d_hist type: {type(occ_2d_hist)}")
+    print(f"occ_2d_canvas: {occ_2d_canvas}")
+    print(f"occ_2d_canvas type: {type(occ_2d_canvas)}")
 
-    #n_bins_x = occ_2d_hist.GetNbinsX()
-    #n_bins_y = occ_2d_hist.GetNbinsY()
-    #print(f"n_bins_x: {n_bins_x}")
-    #print(f"n_bins_y: {n_bins_y}")
+    # n_bins_x = occ_2d_hist.GetNbinsX()
+    # n_bins_y = occ_2d_hist.GetNbinsY()
+    # print(f"n_bins_x: {n_bins_x}")
+    # print(f"n_bins_y: {n_bins_y}")
 
-    occ_2d_hist.Draw()
+    # Draw canvas
+    occ_2d_canvas.Draw()
 
+    # Ask for user input to prevent python from exiting immediately
     user_input = input("Press enter to continue... ")
 
 if __name__ == "__main__":
