bf6bbae88f308a3dfb5c318d4dd52fda7188a073
Caleb James Smith <colorado.caleb.smith@gmail.com>
2024-02-21 14:31:09 -0600

    Load 2D histogram from canvas and get number of bins.

--- python/analyze_occupancy.py
+++ python/analyze_occupancy.py
@@ -28,13 +28,40 @@ def main():
         f.Close()
         return
     
-    print(f"occ_2d_canvas: {occ_2d_canvas}")
-    print(f"occ_2d_canvas type: {type(occ_2d_canvas)}")
+    # Get list of primitives from canvas
+    primitives = list(occ_2d_canvas.GetListOfPrimitives())
+    
+    print("Canvas:")
+    print(f" - occ_2d_canvas: {occ_2d_canvas}")
+    print(f" - occ_2d_canvas type: {type(occ_2d_canvas)}")
+    print(f" - primitives: {primitives}")
+    
+    # Print primitives
+    print("Primitives:")
+    for object in primitives:
+        print(f" - {type(object)}: {object}")
 
-    # n_bins_x = occ_2d_hist.GetNbinsX()
-    # n_bins_y = occ_2d_hist.GetNbinsY()
-    # print(f"n_bins_x: {n_bins_x}")
-    # print(f"n_bins_y: {n_bins_y}")
+    # Load 2D occupancy histogram
+    occ_2d_hist_name = "D_B(0)_O(0)_H(0)_Occ2D_Chip(15)"
+    occ_2d_hist = occ_2d_canvas.GetPrimitive(occ_2d_hist_name)
+
+    # Check for errors when loading histogram
+    if not occ_2d_hist:
+        print(f"ERROR: Unable to load this histogram: {occ_2d_hist_name}")
+        f.Close()
+        return
+    
+    # Get number of bins (x, y) for 2D histogram
+    n_bins_x = occ_2d_hist.GetNbinsX()
+    n_bins_y = occ_2d_hist.GetNbinsY()
+    n_bins_total = n_bins_x * n_bins_y
+    
+    print("Histogram:")
+    print(f" - occ_2d_hist: {occ_2d_hist}")
+    print(f" - occ_2d_hist type: {type(occ_2d_hist)}")
+    print(f" - n_bins_x: {n_bins_x}")
+    print(f" - n_bins_y: {n_bins_y}")
+    print(f" - n_bins_total: {n_bins_total}")
 
     # Draw canvas
     occ_2d_canvas.Draw()
