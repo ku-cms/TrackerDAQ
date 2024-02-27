fe3671fc20d634b8525a9edd0c9941f5b6cebcbd
Caleb James Smith <colorado.caleb.smith@gmail.com>
2024-02-27 16:31:31 -0600

    Update get plots script for elink_testing_v2 area.

--- scripts/getPlots.sh
+++ scripts/getPlots.sh
@@ -11,7 +11,8 @@
 #rsync -az kucms@kucms.phsx.ku.edu:/home/kucms/TrackerDAQ/croc/Ph2_ACF/DAQSettings_v3/plots/ raw_plots_PortCard_RD53B
 #rsync -az kucms@kucms.phsx.ku.edu:/home/kucms/TrackerDAQ/modules/Ph2_ACF/DAQSettings_v1/plots/ raw_plots_PortCard_Module
 
+# kucms
 rsync -az kucms@kucms.phsx.ku.edu:/home/kucms/TrackerDAQ/elink_testing_v1/Ph2_ACF/DAQSettings_v1/plots/ plots_elink_testing_v1
+rsync -az kucms@kucms.phsx.ku.edu:/home/kucms/TrackerDAQ/elink_testing_v2/Ph2_ACF/DAQSettings_v1/plots/ plots_elink_testing_v2
 rsync -az kucms@kucms.phsx.ku.edu:/home/kucms/TrackerDAQ/modules/Ph2_ACF/DAQSettings_v1/plots/ plots_modules
-
 
