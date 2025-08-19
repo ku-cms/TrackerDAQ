59a4d73ce9e6d204cd6558302a78192e461ba40b
Caleb James Smith <colorado.caleb.smith@gmail.com>
2025-08-19 12:14:43 -0500

    Update getPlots.sh: add rsync for kucms-02 and remove commands for kucms-01.

--- scripts/getPlots.sh
+++ scripts/getPlots.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 # getPlots.sh
 
-# kucms-01
-#rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/update/Ph2_ACF/DAQSettings_v1/plots/ raw_plots_RD53A
-#rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/RD53A/Ph2_ACF/DAQSettings_v1/plots/ raw_plots_RD53A_v2
-#rsync -az kucms@kucms-01.phsx.ku.edu:/home/kucms/TrackerDAQ/croc/Ph2_ACF/DAQSettings_v1/plots/ raw_plots_RD53B
-
 # kucms
-#rsync -az kucms@kucms.phsx.ku.edu:/home/kucms/TrackerDAQ/update/Ph2_ACF/DAQSettings_v1/plots/ raw_plots_RD53A
-#rsync -az kucms@kucms.phsx.ku.edu:/home/kucms/TrackerDAQ/croc/Ph2_ACF/DAQSettings_v3/plots/ raw_plots_PortCard_RD53B
-#rsync -az kucms@kucms.phsx.ku.edu:/home/kucms/TrackerDAQ/modules/Ph2_ACF/DAQSettings_v1/plots/ raw_plots_PortCard_Module
+#rsync -az kucms@kucms.phsx.ku.edu:/home/kucms/TrackerDAQ/elink_testing_v1/Ph2_ACF/DAQSettings_v1/plots/ plots_elink_testing_v1
+#rsync -az kucms@kucms.phsx.ku.edu:/home/kucms/TrackerDAQ/elink_testing_v2/Ph2_ACF/DAQSettings_v1/plots/ plots_elink_testing_v2
+#rsync -az kucms@kucms.phsx.ku.edu:/home/kucms/TrackerDAQ/modules/Ph2_ACF/DAQSettings_v1/plots/ plots_modules
 
-# kucms
-rsync -az kucms@kucms.phsx.ku.edu:/home/kucms/TrackerDAQ/elink_testing_v1/Ph2_ACF/DAQSettings_v1/plots/ plots_elink_testing_v1
-rsync -az kucms@kucms.phsx.ku.edu:/home/kucms/TrackerDAQ/elink_testing_v2/Ph2_ACF/DAQSettings_v1/plots/ plots_elink_testing_v2
-rsync -az kucms@kucms.phsx.ku.edu:/home/kucms/TrackerDAQ/modules/Ph2_ACF/DAQSettings_v1/plots/ plots_modules
+# kucms-02
+rsync -az kucms@PHSX-kucms-02.phsx.ku.edu:/home/kucms/TrackerDAQ/elink_testing_v3/Ph2_ACF/DAQSettings_v1/plots/ plots_elink_testing_v3
 
